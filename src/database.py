b9b15222089e2c5f850c509d2aa912b6dbc8580c
Raphael Golubev <raphael.golubev@gmail.com>
2024-04-23 19:59:45 +0300

    навел порядок в database.py

--- src/database.py
+++ src/database.py
@@ -1,23 +1,36 @@
-from sqlalchemy import Column, Integer
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.ext.asyncio import create_async_engine
-from sqlalchemy.orm import sessionmaker
+from datetime import datetime
+from typing import Annotated
+from uuid import UUID
+from sqlalchemy import text
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlalchemy.orm import DeclarativeBase, mapped_column
 
 from src.config import database_settings
 
 
-engine_async = create_async_engine(database_settings.asyncpg_url, echo=True)
+class Base(DeclarativeBase):
+    pass
 
-Base = declarative_base()
 
-async_session = sessionmaker(
-    bind=engine_async, 
-    class_=AsyncSession,
-    expire_on_commit=False
+class Annotations:
+    """ Класс с аннотациями для переиспользуемости кода в объявлении таблиц """
+    primary_id = Annotated[UUID, mapped_column(primary_key=True)]
+    created_at = Annotated[datetime, mapped_column(server_default=text("TIMEZONE('utc', now())"))]
+    updated_at = Annotated[datetime, mapped_column(
+        server_default=text("TIMEZONE('utc', now())"), 
+        onupdate=text("TIMEZONE('utc', now())")
+    )]
+
+
+async_engine = create_async_engine(
+    database_settings.asyncpg_url, 
+    echo=True,
+    pool_size=10,
+    max_overflow=10,
 )
+async_session_factory = async_sessionmaker(async_engine)
 
 
-async def create_tables() -> None:
-    async with engine_async.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
+def create_tables():
+    Base.metadata.drop_all(async_engine)
+    Base.metadata.create_all(async_engine)
