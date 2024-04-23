c9bd52e76d2814ca7f568e00385b6033727c81e9
Raphael Golubev <raphael.golubev@gmail.com>
2024-04-23 20:00:50 +0300

    добавил модель юзера

---
+++ src/apps/auth/model.py
@@ -0,0 +1,21 @@
+from enum import Enum
+from sqlalchemy.orm import Mapped, mapped_column
+
+from src.database import Base, Annotations
+import enum
+
+
+class Role(Enum):
+    GUEST = 'GUEST'
+    USER = 'USER'
+    MODERATOR = 'MODERATOR'
+    ADMIN = 'ADMIN'
+
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id: Mapped[Annotations.primary_id]
+    role: Mapped[Role] = mapped_column(default=Role.USER)
+    created_at: Mapped[Annotations.created_at]
+    updated_at: Mapped[Annotations.updated_at]
